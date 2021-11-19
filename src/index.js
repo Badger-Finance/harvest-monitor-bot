a57a77470940bd8e8f61d19e3e8d6411b339599f
Ayush Shukla <shuklaayush247@gmail.com>
2021-11-19 10:58:20 +0530

    refactor: throw on errors

--- src/index.js
+++ src/index.js
@@ -7,25 +7,21 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.once("ready", async () => {
   console.log("Running...");
-  try {
-    const guild = client.guilds.cache.get(GUILD_ID);
-    const channel = guild.channels.cache.get(CHANNEL_ID);
-    const payloads = await getHarvestTables(Object.values(CHAIN_IDS));
-    // TOOD: Don't actually need pinned messages
-    const pinnedMessages = await channel.messages.fetchPinned();
-    const messages = await pinnedMessages.last(2);
-    if (messages.length === 0) {
-      await channel.send(payloads.pools);
-      await channel.send(payloads.harvests);
-    } else if (messages.length === 1) {
-      await messages[0].edit(payloads.pools);
-      await channel.send(payloads.harvests);
-    } else {
-      await messages[1].edit(payloads.pools);
-      await messages[0].edit(payloads.harvests);
-    }
-  } catch (error) {
-    console.error(error);
+  const guild = client.guilds.cache.get(GUILD_ID);
+  const channel = guild.channels.cache.get(CHANNEL_ID);
+  const payloads = await getHarvestTables(Object.values(CHAIN_IDS));
+  // TOOD: Don't actually need pinned messages
+  const pinnedMessages = await channel.messages.fetchPinned();
+  const messages = await pinnedMessages.last(2);
+  if (messages.length === 0) {
+    await channel.send(payloads.pools);
+    await channel.send(payloads.harvests);
+  } else if (messages.length === 1) {
+    await messages[0].edit(payloads.pools);
+    await channel.send(payloads.harvests);
+  } else {
+    await messages[1].edit(payloads.pools);
+    await messages[0].edit(payloads.harvests);
   }
   console.log("Done!");
   client.destroy();
